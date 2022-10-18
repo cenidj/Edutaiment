44d6e453745f902a4faaea40374abd3b5ec10acb
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-10-18 18:38:16 -0400

    Add option to choose table of x number between 2 to 12

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -1,21 +1,39 @@
-//
-//  ContentView.swift
-//  Edutainment
-//
-//  Created by Cesario Nivar on 10/18/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tableIndex: Int = 0
+    @State private var questions: Int = 0
+    
+    private var table: Int {
+        tableIndex + 2
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Table Options") {
+                    VStack(alignment: .leading){
+                        Text("Which table do you want to play with?")
+                        Picker("Which table do you want", selection: $tableIndex) {
+                            ForEach(2..<13) {
+                                Text("\($0)")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                }
+                
+                Section("Table of number \(table)") {
+                    VStack(alignment: .center) {
+                        ForEach(1..<13) {
+                            Text("\($0) x \(table) = \($0 * (table))")
+                        }
+                    }
+                }
+                
+            }
+            .navigationTitle("Edutainment")
         }
-        .padding()
     }
 }
 
