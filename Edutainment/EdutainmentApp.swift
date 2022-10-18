9abd7cd93b87df7f99149584d85fee8b17dbb6dd
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-10-18 15:25:23 -0400

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Cesario Nivar on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
